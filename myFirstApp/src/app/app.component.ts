342572b1d0a2dd6f2a81f89be8dfae6d9df93295
fmaslim <franky.maslim@yahoo.com>
2018-02-08 08:29:35 -0500

    Added Interface, classes, and inheritance

--- myFirstApp/src/app/app.component.ts
+++ myFirstApp/src/app/app.component.ts
@@ -1,5 +1,54 @@
 import { Component } from '@angular/core';
 
+// Decorator
+function lesson (target) {
+  Object.defineProperty(target.prototype, 'lesson', {value: () => 'Angular 2'});
+}
+
+interface IHuman {
+  hasLegs: boolean;
+  hasHands: boolean;
+
+  name?: Function;
+  isLate?(time: Date): Function;
+
+}
+
+// Adding a new class
+class Person implements IHuman {
+  firstName = 'John';
+  lastName = 'Doe';
+  hasHands = true;
+  hasLegs = true;
+
+  constructor (firstname, lastname) {
+    this.firstName = firstname;
+    this.lastName = lastname;
+  }
+
+  name() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  whoAreYou() {
+    return `Hi, I'm ${this.name()}`;
+  }
+}
+
+@lesson
+class Student extends Person {
+  course: string;
+
+  constructor (firstname, lastname, course) {
+    super(firstname, lastname);
+    this.course = course;
+  }
+
+  whoAreYou() {
+    return `${super.whoAreYou()} and I'm studying ${this.course}`;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +56,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'The world of Angular 5';
+
+  p = new Student('Franky', 'Maslim', 'Computer Science');
+  name = this.p.whoAreYou();
 }
+
